a54ea53e847b386abeb3e041e238794d05d9872d
rubysaltbush <stephenseruby@gmail.com>
2024-01-11 11:29:19 +1100

    fixed few version numbers in comments

--- scripts/install_dependencies.R
+++ scripts/install_dependencies.R
@@ -2,8 +2,8 @@
 # R version for original analysis 4.3.0
 
 install.packages("tidyverse") # for data manipulation, run version 2.0.0
-install.packages("sf") # for spatial data, run version 1.0-12
-install.packages("terra") # does both raster and vector data, compatible with changes to R spatial data, v1.7-39
+install.packages("sf") # for spatial data, run version 1.0-14
+install.packages("terra") # does both raster and vector data, compatible with changes to R spatial data, v1.7-55
 install.packages("ape") # basic package for phylogenetic trees and analysis, v5.7-1
 install.packages("phytools") # package for phylogenetic visualisations, v2.0-3
 install.packages("phylolm") # phylogenetic logistic regressions, v2.6.2
